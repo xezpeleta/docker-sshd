792eeea8d3d23cf9052420eeae462865134e662e
xezpeleta <xezpeleta@gmail.com>
2017-12-04 13:21:03 +0100

    Pubkey file(s) or directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@
 # $AUTHORIZED_KEYS
 # $MOTD
 
-# Add custom ssh pubkeys
+# Add custom ssh pubkeys. Three options:
+# - SSH pubkey: "ssh-rsa AAAF4fxxx...."
+# - SSH pubkey file: "/my-id-rsa.pub"
+# - Directory with one or more SSH pubkey files: "/my-pubkeys"
 if [ ! -z "$AUTHORIZED_KEYS" ]
 then
   if [ ! -d /root/.ssh ]
@@ -12,7 +15,18 @@ then
     mkdir -m 700 /root/.ssh
   fi
   
-  echo "$AUTHORIZED_KEYS" >> /root/.ssh/authorized_keys
+  # Directory with pubkey files
+  if [ -d "$AUTHORIZED_KEYS" ]
+  then
+    cd "$AUTHORIZED_KEYS" && cat * >> /root/.ssh/authorized_keys
+  # SSH pubkey file
+  elif [ -f "$AUTHORIZED_KEYS" ]
+  then
+    cat "$AUTHORIZED_KEYS" >> /root/.ssh/authorized_keys
+  # SSH pubkey
+  else
+    echo "$AUTHORIZED_KEYS" >> /root/.ssh/authorized_keys
+  fi
 fi
 
 # Set permissions
