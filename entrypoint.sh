e8106740974685fe36413ec725693963ce59f145
xezpeleta <xezpeleta@gmail.com>
2017-12-05 15:50:30 +0100

    Added new environment variables (SSH_USER, SSH_PASSWORD, SSH_HOME, SSH_SHELL)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,57 @@
 #! /bin/bash
 
-# Optional environment variables
-# $AUTHORIZED_KEYS
-# $MOTD
+# Supported environment variables:
+#  $MOTD
+#  $SSH_USER
+#  $SSH_PASSWORD
+#  $SSH_HOME
+#  $SSH_SHELL
+#  $AUTHORIZED_KEYS
+
+# Default user = root
+if [ -z "$SSH_USER" ]
+then
+  SSH_USER="root"
+else
+  # Create user $SSH_USER (only if it does not exist)
+  id "$SSH_USER" > /dev/null 2> /dev/null || adduser "$SSH_USER" -s /bin/bash -D -g ""
+  # Disable password (unlocked)
+  usermod -p '*' "$SSH_USER"
+fi
+
+# Set password (disabled by default)
+if [ ! -z "$SSH_PASSWORD" ]
+then
+   # Change root password and allow SSH root login
+   if [ "$SSH_USER" == "root" ]
+   then
+     echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" |passwd root
+     sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config
+   # Change user password
+   else
+     echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" |passwd $SSH_USER
+   fi
+fi
+
+# Set home
+if [ -z "$SSH_HOME" ]
+then
+  # Read the default home
+  SSH_HOME=$( getent passwd "$SSH_USER" | cut -d: -f6 )
+else
+  # Change/move home directory
+  usermod -m -d "$SSH_HOME" "$SSH_USER"
+fi
+
+# Set shell
+if [ -z "$SSH_SHELL" ]
+then
+  # Read the default shell
+  SSH_SHELL=$( getent passwd "$SSH_USER" | cut -d: -f7 )
+else
+  # Set custom shell
+  usermod -s "$SSH_SHELL" "$SSH_USER"
+fi
 
 # Add custom ssh pubkeys. Three options:
 # - SSH pubkey: "ssh-rsa AAAF4fxxx...."
@@ -10,34 +59,35 @@
 # - Directory with one or more SSH pubkey files: "/my-pubkeys"
 if [ ! -z "$AUTHORIZED_KEYS" ]
 then
-  if [ ! -d /root/.ssh ]
+  # Check if .ssh directory exists
+  if [ ! -d "$SSH_HOME/.ssh" ]
   then
-    mkdir -m 700 /root/.ssh
+    mkdir -p -m 700 $SSH_HOME/.ssh
   fi
-  
+
   # Directory with pubkey files
   if [ -d "$AUTHORIZED_KEYS" ]
   then
-    cd "$AUTHORIZED_KEYS" && cat * >> /root/.ssh/authorized_keys
+    cd "$AUTHORIZED_KEYS" && cat * > $SSH_HOME/.ssh/authorized_keys
   # SSH pubkey file
   elif [ -f "$AUTHORIZED_KEYS" ]
   then
-    cat "$AUTHORIZED_KEYS" >> /root/.ssh/authorized_keys
+    cat "$AUTHORIZED_KEYS" > $SSH_HOME/.ssh/authorized_keys
   # SSH pubkey
   else
-    echo "$AUTHORIZED_KEYS" >> /root/.ssh/authorized_keys
+    echo "$AUTHORIZED_KEYS" > $SSH_HOME/.ssh/authorized_keys
   fi
 fi
 
 # Set permissions
-if [ -d /root/.ssh ]
+if [ -d "$SSH_HOME/.ssh" ]
 then
-  chown -R root:root /root/.ssh
-  chmod 700 /root/.ssh
-  
-  if [ -f /root/.ssh/authorized_keys ]
+  chown -R $SSH_USER:$SSH_USER $SSH_HOME/.ssh
+  chmod 700 $SSH_HOME/.ssh
+
+  if [ -f "$SSH_HOME/.ssh/authorized_keys" ]
   then
-    chmod 600 /root/.ssh/authorized_keys
+    chmod 600 $SSH_HOME/.ssh/authorized_keys
   fi
 fi
 
@@ -48,4 +98,4 @@ then
 fi
 
 # Run sshd service
-/usr/sbin/sshd -D
+/usr/sbin/sshd -Dd
